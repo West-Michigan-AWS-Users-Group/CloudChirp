868527a733cecc172291ebd0c34aa3a79308eb9c
tnielsen2 <trentnielsen84@gmail.com>
2023-05-05 10:04:37 -0400

    Fix bucket/domain name

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,10 @@
 import os
 
 # AWS variables
-aws_account_name = os.environ.get('AWS_ACCOUNT_NAME')
 app_environment = os.environ.get('APP_ENVIRONMENT')
-aws_account_tld = os.environ.get('APP_DNS_DOMAIN')
+aws_account_tld = os.environ.get('APP_DNS_DOMAIN')[:-1]
 stack_name = 'cloudchirp'
-s3_bucket_name = f'{app_environment}-{stack_name}.{aws_account_name}.{aws_account_tld}'
+s3_bucket_name = f'{app_environment}-{stack_name}.{aws_account_tld}'
 
 # Site locations - sorted by abbreviation name
 site_locations = [
